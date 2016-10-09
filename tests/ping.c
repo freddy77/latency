9ceea7f92e6d38f80c7a01b7ee1168a3fcdd633a
Frediano Ziglio <fziglio@redhat.com>
2016-10-09 09:17:20 +0100

    Add ping test
    Test that ping utility works
    
    Signed-off-by: Frediano Ziglio <fziglio@redhat.com>

---
+++ tests/ping.c
@@ -0,0 +1,12 @@
+/* check that ping is working */
+#include "common.h"
+
+int main(void)
+{
+	printf("Testing ping is working\n");
+
+	launch_latency("10 100M");
+	assert(system("ping -c1 192.168.127.1") == 0);
+	kill_latency();
+	return 0;
+}
